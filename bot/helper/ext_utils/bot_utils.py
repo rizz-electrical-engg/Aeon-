71250887f135dcd1126262d9e24c75c5a640d5ed
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-11-11 08:58:57 -0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -248,7 +248,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = "<b>Powered by Aeon</b>\n\n"
+    msg = "<b>Powered by 𝙍𝙞𝙯𝙯𑲭</b>\n\n"
     button = None
     tasks = len(download_dict)
     current_time = get_readable_time(time() - bot_start_time)
